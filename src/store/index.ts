f6e372982145ed6c114d5706a86be1456c72a0ae
Yuvraj210103 <ys146228@gmail.com>
2024-02-03 17:12:22 +0530

    feat: created authUser slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
 import { create } from "zustand";
 
+interface IAuthUser {
+  AuthUserAuthenticated: boolean;
+  AuthUserRole: "admin" | "faculty" | "student";
+}
+
 interface SessionState {
   component: string;
   setComponent: (component: string) => void;
+  authUser: IAuthUser;
+  setAuthUser: (authUser: IAuthUser) => void;
 }
 
 export const useComponentStore = create<SessionState>((set) => ({
   component: "Classroom",
-  setComponent: (component: string) => set({ component }),
+  setComponent: (component: string) =>
+    set((state) => ({ ...state, component })),
+  authUser: { AuthUserAuthenticated: false, AuthUserRole: "admin" },
+  setAuthUser: (data) => set((state) => ({ ...state, authUser: data })),
 }));
