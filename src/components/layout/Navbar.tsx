112225d41d0efed1c8fa8f92ce0730594b8a9e70
Shivam <prajapatishivam11203@gmail.com>
2024-02-08 16:29:07 +0530

    Add useSessionStore hook to Navbar component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,17 +1,19 @@
+"use client"
 import { Bell, MessageSquare } from 'lucide-react';
 import React from 'react';
 
 import { ToggleMode } from '../ToggleMode';
 import { UserAvatar } from '../UserAvatar';
+import { useSessionStore } from '@/store';
 
 const Navbar = () => {
-  const session = true;
+  const { authUser } = useSessionStore();
   return (
     <div className="sticky top-0 z-50 flex h-[60px] w-full items-center justify-between bg-white px-5 py-2 shadow-md dark:bg-gray-950 sm:px-12 lg:px-20">
       <div className="text-2xl font-bold leading-relaxed">Attendify</div>
       <div className="flex items-center gap-x-3 sm:gap-x-5">
         <ToggleMode />
-        {session && (
+        {authUser.AuthUserAuthenticated && (
           <div className="flex items-center gap-x-3 sm:gap-x-5">
             <div className="cursor-pointer">
               <Bell />
