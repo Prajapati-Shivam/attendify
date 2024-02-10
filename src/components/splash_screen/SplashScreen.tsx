0b8fb3e6420beecc90078807b053634b13719844
Yuvraj210103 <ys146228@gmail.com>
2024-02-10 10:31:26 +0530

    feat: added new logo

--- src/components/splash_screen/SplashScreen.tsx
+++ src/components/splash_screen/SplashScreen.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const SplashScreen = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-surfaceLight dark:bg-surfaceDark">
-      <img src="/assets/images/logo.jpeg" alt="" className="animate-bounce" />
+      <img
+        src="/assets/images/logo.png"
+        alt=""
+        className="animate-bounce"
+        style={{ width: '400px' }}
+      />
     </div>
   );
 };
