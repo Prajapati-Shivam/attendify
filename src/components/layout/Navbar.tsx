68600161309012e46c915f2ff713963342267a96
Yuvraj210103 <ys146228@gmail.com>
2024-05-16 16:08:46 +0530

    feat: added logout option in navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,21 @@
 'use client';
 
-import React from 'react';
+import { LogOut } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 import { ConstAppDetails } from '@/constants/ConstAppDetails';
 import { useSessionStore } from '@/store';
 
+import ConfirmDialog from '../common/dialogs/ConfirmDialog';
 import { ToggleMode } from '../ToggleMode';
 import { UserAvatar } from '../UserAvatar';
 
 const Navbar = () => {
-  const { authUser, institute } = useSessionStore();
+  const { authUser, institute, userSignOut } = useSessionStore();
+  const router = useRouter();
+
+  const [logoutConfirmModal, setLogoutConfirmModal] = useState(false);
   return (
     <div className="sticky top-0 z-50 flex h-[60px] w-full items-center justify-between bg-white px-5 py-2 shadow-md dark:bg-gray-950 sm:px-12 lg:px-20">
       <div
@@ -18,6 +24,12 @@ const Navbar = () => {
         {ConstAppDetails.APP_NAME}
       </div>
       <div className="flex items-center gap-x-3 sm:gap-x-5">
+        {authUser.AuthUserAuthenticated && (
+          <LogOut
+            className="cursor-pointer"
+            onClick={() => setLogoutConfirmModal(true)}
+          />
+        )}
         <ToggleMode />
         {((authUser.AuthUserAuthenticated &&
           authUser.AuthUserRole === 'admin' &&
@@ -25,12 +37,6 @@ const Navbar = () => {
           (authUser.AuthUserAuthenticated &&
             authUser.AuthUserRole !== 'admin')) && (
           <div className="flex items-center gap-x-3 sm:gap-x-5">
-            {/* <div className="cursor-pointer">
-              <Bell />
-            </div>
-            <div className="cursor-pointer">
-              <MessageSquare />
-            </div> */}
             <div className="flex items-center gap-2">
               <UserAvatar user={authUser.AuthUserRole} />
               <span className="hidden sm:block">
@@ -40,6 +46,16 @@ const Navbar = () => {
             </div>
           </div>
         )}
+        <ConfirmDialog
+          positiveCallback={() => {
+            userSignOut();
+            router.push('/login');
+          }}
+          open={logoutConfirmModal}
+          setOpened={setLogoutConfirmModal}
+        >
+          <div>Are you sure to logout?</div>
+        </ConfirmDialog>
       </div>
     </div>
   );
